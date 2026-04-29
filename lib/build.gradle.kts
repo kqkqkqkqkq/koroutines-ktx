1b3d64072b5cb5207482159f18107ec4179f07ea
YungOleg <bilk5899@gmail.com>
2026-04-29 22:23:38 +0700

    init

---
+++ lib/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    `java-library`
+    id("maven-publish")
+    id("org.jetbrains.dokka") version "2.2.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation(libs.junit.jupiter.engine)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    api(libs.commons.math3)
+    implementation(libs.guava)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+}
+
+dokka {
+    dokkaPublications.html {
+        moduleName.set("Koroutine KTX")
+        moduleVersion.set(project.version.toString())
+        outputDirectory.set(layout.buildDirectory.dir("dokka/html"))
+        outputDirectory.set(rootDir.resolve("docs/api/v0.1"))
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+            groupId = "com.github.kqkqkqkqkq"
+            artifactId = "koroutines-ktx"
+            version = "1.0.0"
+        }
+    }
+}
---
+++ lib/src/main/kotlin/ru/koroutines/Library.kt
@@ -0,0 +1,10 @@
+package ru.koroutines
+
+/**
+ * A class to use as a library.
+ */
+class Library {
+    fun someLibraryMethod(): Boolean {
+        return true
+    }
+}
